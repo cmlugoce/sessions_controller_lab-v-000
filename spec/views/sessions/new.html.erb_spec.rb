263de3b5fbadfa3cf1f5195eb3ba37e5873822e2
Cristina Lugo <cmlugoce@syr.edu>
2018-07-31 00:45:24 +0000

    automatically backed up by learn

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'sessions/new.html.erb'. type: :views do
 
 
-   it 'includes a form_tag' do 
+   it 'includes a form_tag' do
      visit 'sessions/new'
      expect(page).to have_selector("form")
 		expect(page).to have_field(:name)
