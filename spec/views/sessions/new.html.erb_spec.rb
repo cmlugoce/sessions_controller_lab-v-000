fe3bf5adf9b4e3fd105bd463f57a0c0edfc3e4e3
Cristina Lugo <cmlugoce@syr.edu>
2018-07-31 00:55:29 +0000

    automatically backed up by learn

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe "sessions/new",:type => :view do
    it 'has a form with a name field' do
 
      #expect(rendered).to have_selector("form_tag")
-		expect(rendered).to have_field(:name)
+		expect(rendered).to match(:name)
 	end
 end
