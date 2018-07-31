64b9498f39035accf47438924f925a8b421b22a5
Cristina Lugo <cmlugoce@syr.edu>
2018-07-31 00:48:30 +0000

    automatically backed up by learn

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "sessions/new.html.erb", type: :view do
 
 
    it 'has a form with a name field' do
-      'sessions/new'
+
      expect(page).to have_selector("form")
 		expect(page).to have_field(:name)
 	end
